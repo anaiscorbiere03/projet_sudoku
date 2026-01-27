6b2270a831dc2a5a60313b0a92504ba169793453
Anais Corbière <anais.corbiere@etu.minesparis.psl.eu>
2026-01-27 20:58:50 +0100

    solveur de sudoku

--- src/projet_sudoku/main.py
+++ src/projet_sudoku/main.py
@@ -1,2 +1,17 @@
+import os
+from projet_sudoku.solver import Sudoku
+
 def main() -> None:
-    print("Hello from projet-sudoku!")
+    chemin = os.path.join(os.path.dirname(__file__), "exemple.txt")
+    sudoku = Sudoku(chemin)
+    if sudoku.resoudre():
+        print("Grille résolue :\n")
+        for i, ligne in enumerate(sudoku.grille):
+            if i % 3 == 0 and i != 0:
+                print("-------+--------+-------")
+            ligne_affiche = " ".join(str(x) if x != 0 else "." for x in ligne)
+            blocs = [ligne_affiche[0:6], ligne_affiche[6:12], ligne_affiche[12:]]
+            print(" | ".join(blocs))
+        print()
+    else:
+        print("Aucune solution trouvée.")
